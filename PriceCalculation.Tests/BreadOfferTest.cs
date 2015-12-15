2c1d1efbbf328bcc7569dad736d135ee51f91d6a
Rob Bell <me@rob-bell.net>
2015-12-15 22:45:35 +0000

    Complete offer seperation

---
+++ PriceCalculation.Tests/BreadOfferTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using PriceCalculation.Tests;
+
+namespace PriceCalculation.Tests
+{
+    [TestFixture]
+    public class BreadOfferTest
+    {
+        private IOffer offer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            offer = new BreadOffer();
+        }
+
+        [Test]
+        public void BreadIsHalfPriceWhenTwoButtersArePurchased()
+        {
+            var products = new[] { Product.Butter, Product.Butter, Product.Bread };
+
+            Assert.That(offer.GetDiscount(products), Is.EqualTo(0.50m));
+        }
+
+        [Test]
+        public void BreadOfferDoesNotApplyToExtraBread()
+        {
+            var products = new[] { Product.Butter, Product.Butter, Product.Bread, Product.Bread };
+
+            Assert.That(offer.GetDiscount(products), Is.EqualTo(0.50m));
+        }
+
+        [Test]
+        public void BreadOfferCanBeAppliedMultipleTimes()
+        {
+            var products = new[]
+            {
+                Product.Butter, Product.Butter, Product.Bread,
+                Product.Butter, Product.Butter, Product.Bread
+            };
+
+            Assert.That(offer.GetDiscount(products), Is.EqualTo(1.00m));
+        }
+    }
+}
+
+public class BreadOffer : IOffer
+{
+    private const int noOfButterRequiredForOffer = 2;
+
+    public decimal GetDiscount(IEnumerable<Product> products)
+    {
+        var noOfButter = products.Count(p => p == Product.Butter);
+        var noOfBread = products.Count(p => p == Product.Bread);
+
+        var noOfTimesOfferCanApply = noOfButter / noOfButterRequiredForOffer;
+        var noOfTimesToApplyOffer = Math.Min(noOfBread, noOfTimesOfferCanApply);
+
+        return noOfTimesToApplyOffer * (Product.Bread.Price / 2);
+    }
+}
---
+++ PriceCalculation.Tests/MilkOfferTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace PriceCalculation.Tests
+{
+    [TestFixture]
+    public class MilkOfferTest
+    {
+        private IOffer offer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            offer = new MilkOffer();
+        }
+
+        [Test]
+        public void FourthMilkIsFreeWhenThreeMilksArePurchased()
+        {
+            var products = new[] { Product.Milk, Product.Milk, Product.Milk, Product.Milk };
+
+            Assert.That(offer.GetDiscount(products), Is.EqualTo(1.15m));
+        }
+
+        [Test]
+        public void MilkOfferDoesNotApplyToExtraMilk()
+        {
+            var products = new[]
+            {
+                Product.Milk, Product.Milk, Product.Milk, Product.Milk,
+                Product.Milk
+            };
+
+            Assert.That(offer.GetDiscount(products), Is.EqualTo(1.15m));
+        }
+
+        [Test]
+        public void MilkOfferIsAppliedMultipleTimes()
+        {
+            var products = new[]
+            {
+                Product.Milk, Product.Milk, Product.Milk, Product.Milk,
+                Product.Milk, Product.Milk, Product.Milk, Product.Milk
+            };
+
+            Assert.That(offer.GetDiscount(products), Is.EqualTo(2.30m));
+        }
+    }
+
+    public class MilkOffer : IOffer
+    {
+        private const int noOfMilkRequiredForOffer = 4;
+
+        public decimal GetDiscount(IEnumerable<Product> products)
+        {
+            var noOfMilk = products.Count(p => p == Product.Milk);
+
+            var noOfTimesToApplyOffer = noOfMilk / noOfMilkRequiredForOffer;
+
+            return noOfTimesToApplyOffer * Product.Milk.Price;
+        }
+    }
+}
